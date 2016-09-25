d09b0d21e985b0fdf15def3c1b8bdc26c4b14357
Marcel de Vries <vriesmarcel@hotmail.com>
2016-09-25 20:45:02 +0200

    Added Feature toggle table

---
+++ MvcMusicStore/Models/FeatureTogle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class FeatureTogle
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
