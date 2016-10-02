ba6885157bec4cdcbf6efbfedcbf70ed57b507e4
Marcel de Vries <vriesmarcel@hotmail.com>
2016-10-02 22:19:18 +0200

    Add trafic management scripts

---
+++ Provisioning/WebApps/SetSlotTrafic.ps1
@@ -0,0 +1,12 @@
+﻿param (
+	[string]$webAppName,
+	[string]$productionSlotName,
+	[string]$canarySlotName,
+	[string]$percentage
+)
+Write-Host "Setting slot on webapp $webAppName slotname $canarySlotName to $percentage % trafic"
+$rule = New-Object Microsoft.WindowsAzure.Commands.Utilities.Websites.Services.WebEntities.RampUpRule
+$rule.ActionHostName = "$webAppName-$canarySlotName.azurewebsites.net"
+$rule.ReroutePercentage = $percentage
+$rule.Name = $canarySlotName
+Set-AzureWebsite $webAppName -Slot $productionSlotName -RoutingRules $rule
