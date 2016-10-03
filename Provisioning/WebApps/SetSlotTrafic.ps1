ac259a90ab3fede5995f71ffe9f4242c6cd0ebaf
Marcel de Vries <vriesmarcel@hotmail.com>
2016-10-03 23:12:17 +0200

    changes flighting scripts

--- Provisioning/WebApps/SetSlotTrafic.ps1
+++ Provisioning/WebApps/SetSlotTrafic.ps1
@@ -7,11 +7,11 @@
 Write-Host "Setting slot on webapp $webAppName slotname $canarySlotName to $percentage % trafic"
 $rule = New-Object Microsoft.WindowsAzure.Commands.Utilities.Websites.Services.WebEntities.RampUpRule
 $rule.ActionHostName = "$webAppName-$canarySlotName.azurewebsites.net"
-#$rule.ReroutePercentage = $percentage
+$rule.ReroutePercentage = $percentage
 $rule.Name = $canarySlotName
 $rule.ChangeIntervalInMinutes = 1;
 $rule.ChangeStep = 5;
-#$rule.MinReroutePercentage = 1;
+$rule.MinReroutePercentage = 1;
 $rule.MaxReroutePercentage = $percentage;
 #$rule.ChangeDecisionCallbackUrl = "http://mvcmusicstoretechdays.azurewebsites.net/TestInProduction/GetRoutingDirection"
 
